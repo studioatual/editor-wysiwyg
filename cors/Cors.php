22b18fcea03dd9b806a0dcad014310708f5d561f
Willian Souza <willian.souza@fbssistemas.com.br>
2020-06-08 09:47:16 -0300

    Complete Editor

---
+++ cors/Cors.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        header('Access-Control-Allow-Origin:  *');
+        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+        header('Access-Control-Allow-Methods:  POST, PUT');
+
+        return $next($request);
+        /*
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-Token-Auth, Accept, Origin');
+        */
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * GERADOR DE PACOTES
+ */
+module.exports = require('./FbsEditor').default;
+
+/**
+ * TESTADOR
+ */
+/*
+import FbsEditor from './FbsEditor';
+
+let editor;
+document.body.onload = () => {
+  editor = new FbsEditor('#fbs-editor');
+};
+*/
